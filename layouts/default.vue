1393c45f726dfb03837f3e769f75569e1079bf3d
Matej Kolimár <matej.kolimar@avast.com>
2022-04-24 14:22:50 +0200

    Initial UI commit

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+        <TheSidenav :show="displaySidenav" @close="displaySidenav = false" />
+        <nuxt />
+    </div>
+</template>
+
+<script>
+import TheSidenav from '~/components/Navigation/TheSidenav.vue'
+import TheHeader from '~/components/Navigation/TheHeader.vue'
+
+export default {
+    components: {
+        TheSidenav,
+        TheHeader,
+    },
+    data() {
+        return {
+            displaySidenav: false,
+        }
+    },
+}
+</script>
+
+<style>
+html {
+    font-family: 'Open Sans', 'sans-serif';
+}
+body {
+    margin: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="home-page">
+        <section class="intro">
+            <h1>Get the latest tech news!</h1>
+        </section>
+        <section class="featured-posts">
+            <PostPreview
+                id="1"
+                thumbnail="https://www.wpbeginner.com/wp-content/uploads/2020/04/featuredimageswp-og.png"
+                title="Hello there!"
+                preview-text="This is my first post!" />
+            <PostPreview
+                id="1"
+                thumbnail="https://www.wpbeginner.com/wp-content/uploads/2020/04/featuredimageswp-og.png"
+                title="Hello there!"
+                preview-text="This is my second post!" />
+            <PostPreview
+                id="1"
+                thumbnail="https://www.wpbeginner.com/wp-content/uploads/2020/04/featuredimageswp-og.png"
+                title="Hello there!"
+                preview-text="This is my third post!" />
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IndexPage',
+}
+</script>
+
+<style scoped>
+.intro {
+    height: 300px;
+    position: relative;
+    padding: 30px;
+    box-sizing: border-box;
+    background-position: center;
+    background-image: linear-gradient(rgb(0, 255, 217), rgb(49, 134, 135));
+    background-size: cover;
+}
+
+.intro h1 {
+    position: absolute;
+    top: 10%;
+    left: 5%;
+    width: 90%;
+    font-size: 1.5rem;
+    color: black;
+    background-color: rgb(211, 211, 211);
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 3px 3px 3px black;
+    box-sizing: border-box;
+    border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+    .intro h1 {
+        font-size: 2rem;
+    }
+}
+
+.featured-posts {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+}
+</style>
